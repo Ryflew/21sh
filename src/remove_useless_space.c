489e8765ef0f79b3d9ff27d3aaf83b57c0274f9e
Victor DARMAYAN <vdarmaya@e3r8p16.42.fr>
2017-03-01 18:13:58 +0100

    push du de la fonction de traitement pour les espaces en trop

--- src/remove_useless_space.c
+++ src/remove_useless_space.c
@@ -6,7 +6,35 @@
 /*   By: vdarmaya <vdarmaya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/28 23:21:57 by vdarmaya          #+#    #+#             */
-/*   Updated: 2017/02/28 23:21:58 by vdarmaya         ###   ########.fr       */
+/*   Updated: 2017/03/01 18:12:08 by vdarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+char	*remove_useless_space(char *str)
+{
+	int		i;
+	int		j;
+	int		have_space;
+	char	*out;
+	char	buff[ft_strlen(str)];
+
+	ft_bzero(buff, ft_strlen(str));
+	have_space = 0;
+	j = -1;
+	i = -1;
+	while (str[++i])
+	{
+		if (str[i] != ' ' && str[i] != '\t')
+		{
+			if (have_space && j != -1)
+			{
+				buff[++j] = ' ';
+				have_space = 0;
+			}
+			buff[++j] = str[i];
+		}
+		else
+			have_space = 1;
+	}
+	return (ft_strdup(buff));
+}
