8249d64bccf2444c3c45217d9a51ebbfd6620068
Basile Durst <bdurst2812@gmail.com>
2017-10-06 23:38:26 +0200

    core2.c pour vic

--- srcs/core2.c
+++ srcs/core2.c
@@ -56,6 +56,26 @@ char	**create_cmds_with_tokens(t_list *lexems)
 	return (cmds);
 }
 
+char	is_env_var_to_add(t_list *cmd_tokens)
+{
+	t_list	*tmp;
+	t_token	*token;
+
+	tmp = cmd_tokens;
+	while (tmp)
+	{
+		token = (t_token*)tmp->data;
+		if (TYPE == EQUAL)
+		{
+			if (tmp->prev && tmp->prev->prev && !ft_strcmp(((t_token*)tmp->prev->prev->data)->value, "export"))
+				;// VICTOR doit implémenter export et setenv ici mais aussi yolo=ok; export yolo
+			return (0);
+		}
+		tmp = tmp->next;
+	}
+	return (1);
+}
+
 char	**get_cmds(t_list **cmd_tokens, t_sh *sh)
 {
 	t_list	*tmp;
@@ -82,7 +102,7 @@ char	**get_cmds(t_list **cmd_tokens, t_sh *sh)
 			if (tmp)
 				replace_var(last_type, &tmp, sh);
 		}
-		else if (TYPE == TILD)
+		else if (TYPE == TILD || TYPE == TILD_VAR_WORD)
 			replace_tild(token, sh->env);
 		tmp = tmp->next;
 	}
@@ -109,5 +129,7 @@ char	manage_cmds(t_tree *node, t_sh *sh)
 	if (find_env(sh->env, "PATH"))
 		try_add_hashtab(node, sh);
 	treat_history_cmd(node);
+	if (!is_env_var_to_add(node->cmd_tokens))
+		return (0);
 	return (exec_cmds(node, &(sh->env), sh));
 }
